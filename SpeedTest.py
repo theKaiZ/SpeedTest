319b1a2b389476c62c4291c181f1457872ce3a9d
kaizumpfe <kai_zumpfe@yahoo.de>
2021-09-03 08:40:20 +0200

    optimized a little

--- SpeedTest.py
+++ SpeedTest.py
@@ -16,35 +16,43 @@ class SpeedTest():
      if fun.__name__ in self.functions:
         return 1
      self.add_function(fun)
+     print(fun.__name__)
   def add_time(self,fun,t):
      self.has_function(fun)
      self.functions[fun.__name__]['dts'].append(t)
   def stats(self):
      fig = plt.figure()
      ax = fig.add_subplot(111)
+     #num_keys = len(self.functions.keys())
      for n,key in enumerate(self.functions.keys()):
         print(key)
-        print(*self.functions[key]['dts'])
-        ax.bar(n,np.sum(self.functions[key]['dts']))
+        t = time()
+        dts = np.array(self.functions[key]['dts'])
+        print(time()-t,"um das in ein array umzuwandeln")
+        #TODO the thing with the plotting seems to be unpractical. Better check if the fist run is way longer than the
+        #TODO other runs, but this will just indicate the use of jit
+        #if len(dts)> num_keys:
+          #if dts.max() > 2* dts.min():
+             #if len(dts)>100:
+               #plt.figure().add_subplot().bar(np.arange(100),dts[:100])
+
+        #print(*self.functions[key]['dts'])
+        ax.bar(n,np.sum(dts))
      ax.set_xticks(range(len(self.functions.keys())))
      ax.set_xticklabels(self.functions.keys(),rotation=90)
      fig.tight_layout()
      plt.show()
 
 
-def time_runtime(function,min=0.0):
+def time_runtime(function):
     def wrapper(*args, **kwargs):
         if function.__name__ == 'main':
            print("Start der Main function")
-        else:
-           print(function.__name__)
         start = time()
         result = function(*args, **kwargs)
         end = time()
         diff = end-start
-        if diff > min:
-          print("Time to run in seconds: {}".format(end-start))
-        
+
         SpeedTest().add_time(function,diff)  
         if function.__name__ == 'main':
            print("Ende der Main function")  
