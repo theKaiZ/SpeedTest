f9b706b9a06ce19f62e08711208baaef9555f785
the_KaiZ <kai_zumpfe@yahoo.de>
2021-09-13 18:18:05 +0200

    ...

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,9 +1,12 @@
 from time import time
+import matplotlib
+#matplotlib.use("agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
 class SpeedTest():
   functions = {}
+  no_main = True
   def __init__(self):
      pass
      
@@ -50,18 +53,18 @@ class SpeedTest():
               while dtmean < 0.1:
                 dtmean *= 1000
                 umean+=1
-            print("Num runs: {}\n total time: {:.2f}\n max: {:.2f} {}\n min: {:.2f} {}\n mean: {:.2f} {}\n\n".format(len(dts),dts.sum(),dtmax,units[umax],dtmin,units[umin],dtmean,units[umean]))
+            print("Num runs: {}\n total time: {:.2f} s\n max: {:.2f} {}\n min: {:.2f} {}\n mean: {:.2f} {}\n\n".format(len(dts),dts.sum(),dtmax,units[umax],dtmin,units[umin],dtmean,units[umean]))
         ax.bar(n,np.sum(dts))
      ax.set_xticks(range(len(self.functions.keys())))
      ax.set_xticklabels(self.functions.keys(),rotation=90)
      fig.tight_layout()
-     plt.show()
-
+     fig.savefig("test.png")
 
 def time_runtime(function):
     def wrapper(*args, **kwargs):
         if function.__name__ == 'main':
-           print("Start of Main function")
+            print("Start of Main function")
+            SpeedTest().no_main = False
         SpeedTest().has_function(function)
         start = time()
         result = function(*args, **kwargs)
@@ -70,5 +73,7 @@ def time_runtime(function):
         if function.__name__ == 'main':
            print("End of Main function after {:.2f} seconds".format(diff))
            SpeedTest().stats()
+        if SpeedTest().no_main:
+            print(function.__name__,diff,"Sekunden")
         return result
     return wrapper
